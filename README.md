36e7e86455ec105b88df599a3ad19f022ca63a8c
Andrea Jemison <andreadjemison@yahoo.com>
2019-04-14 18:04:39 -0400

    from ghe to github

---
+++ README.md
@@ -0,0 +1,40 @@
+# Jeopardy Project
+I wanted to recreate an authentic jeopardy game 
+
+#Instructions
+Select a dollar amount below one of the categories. Buzz in with the correct response to win that amount, or a wrong response will subtract the amount.
+
+There are Daily Doubles placed at random on the board. Only the player who selects a SPECIAL may answer it. Prior to answering, they must make a wager. The minimum wager is $5 and the maximum wager is the amount they have accumulated in the bank.
+
+Player 1 uses Q to buzz in
+Player 2 uses P to buzz in
+
+* Choose Theme
+* Insert name of Player 1
+    * Player 1 uses Q to buzz in
+* Insert name of Player 2
+    * Player 2 uses P to buzz in
+* Begin game
+
+# Overview
+You will have 15 seconds to respond to each clue. At the end of each 15-second interval, whatever has been typed into the screen will be captured. Do not respond in the form of a question. When the test is over you will be able to compare your responses with the correct responses.
+
+# Project Planning - Trello
+[Trello link](https://trello.com/b/Tst2WNFz/sei-21-jeopardy-game)
+
+# References Used
+[Jeopardy Game Example](http://www.freewebarcade.com/game/jeopardy/)
+
+# Technologies Used
+HTML5
+CSS3
+jQuery
+
+# Next Version
+Computer AI <br>
+3 Players <br>
+2nd round
+
+
+
+
---
+++ app.js
@@ -0,0 +1,154 @@
+// ==============theme song plays on index page load==========
+// $(document).ready(()=>{
+// let song = $("#themesong")
+// song.autoplay = true 
+// song.volume = .8
+// song.play()
+// song.preload()
+
+// //     $('#themesong').on('load', ()=>{
+// //         $(this).play()
+// // })
+// })
+
+
+
+// new Audio('audio/themesong.mp3').play()
+// song.trigger('load')
+
+
+// on click make themes visible and flip over
+$("#choose").click(() => {
+    $(".themes").css("display", "flex")
+    $(".theme1").toggleClass("theme1-active")
+    $(".theme2").toggleClass("theme2-active")
+    $(".theme3").toggleClass("theme3-active")
+})
+
+// $('#one').click( ()=>{
+//     swal({
+//         title: "Name of Player 1",
+//         text: "",
+//         conyent: "input",
+//         // closeOnConfirm: false,
+//         inputPlaceholder: "Player 1"
+//       }
+    //   (input)=> {
+    //     if (input === false) return false;
+        // if (input === "") {
+        //   swal.showInputError("You need to write something!");
+        //   return false
+        // }
+        // swal("Nice!", "You wrote: " + input, "success")
+    // )
+    // })
+
+    // $('.#one').on('click', ()=>{
+    //     event.preventDefault();
+    //     $('.officemodal').toggleClass('is-visible');
+    //   });
+    // $('#officemodal').on()
+
+// $(() => {
+//     for (let i = 0; i < 6; i++) {
+//         $('.cat').append('<div class="categories"></div>')
+//     }
+// })
+
+// $(() => {
+//     for (let i = 0; i < 6; i++) {
+//         $('.row').append('<div class="questions"></div>')
+//         for (let i = 0; i < 5; i++) {
+//             $('.ques').append('<div class="questions"></div>')
+//         }
+//     }
+// })
+
+let officeCat = {[
+    {
+        'name': 'pranks',
+        'questions': [{
+            'value': 100,
+            'question': "In what food did Jim put Dwight's stapler in?",
+            "answers": [{
+                "one": "Pie",
+                "right": false
+            }, {
+                "two": "Jello",
+                "right": true
+            }, {
+                "three": "Pudding",
+                "right": false
+            }, {
+                "four": "Cake",
+                "right": false
+            }]
+        },{
+            'value': 200,
+            'question': "Jim sends faxes to Dwight from who?",
+            "answers": [{
+                "one": "Abe Lincoln",
+                "right": false
+            }, {
+                "two": "A high ranking official from the FBI",
+                "right": false
+            }, {
+                "three": "Hitler",
+                "right": false
+            }, {
+                "four": "Future Dwight",
+                "right": true
+            }]
+        },{
+            'value': 300,
+            'question': "Where does Jim relocate Dwight's desk to?",
+            "answers": [{
+                "one": "The men's bathroom",
+                "right": true
+            }, {
+                "two": "The women's bathroom",
+                "right": false
+            }, {
+                "three": "The roof",
+                "right": false
+            }, {
+                "four": "The elevator",
+                "right": false
+            }]
+        },{
+            'value': 400,
+            'question': "Jim dresses up as who to freak Dwight out?",
+            "answers": [{
+                "one": "Dwight",
+                "right": true
+            }, {
+                "two": "The Sith Lord",
+                "right": false
+            }, {
+                "three": "Pimp",
+                "right": false
+            }, {
+                "four": "The Devil",
+                "right": false
+            }]
+        },{
+            'value': 500,
+            'question': "Jim convinces Dwight he's being recruited by who?",
+            "answers": [{
+                "one": "Secret Service",
+                "right": false
+            }, {
+                "two": "FBI",
+                "right": false
+            }, {
+                "three": "CIA",
+                "right": true
+            }, {
+                "four": "PETA",
+                "right": false
+            }]
+        }
+        ]
+    }
+]}
+
