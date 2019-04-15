c7a23428591843d0dd7f6d88355a51e73cf60418
Andrea Jemison <andreadjemison@yahoo.com>
2019-04-15 06:55:57 -0400

    commit 12

--- app.js
+++ app.js
@@ -18,16 +18,17 @@
 
 
 // on click make themes visible and flip over
-$("#choose").on('click',() => {
+$("#choose").on('click', () => {
     $(".themes").css("display", "flex")
     $(".theme1").toggleClass("theme1-active")
     $(".theme2").toggleClass("theme2-active")
     $(".theme3").toggleClass("theme3-active")
 })
+let score = 0
 
 
 // pranks category
-let officeCat = 
+let officeCat = [
     {
         'name': 'pranks',
         'questions': [{
@@ -45,8 +46,12 @@ let officeCat =
             }, {
                 "d": "Cake",
                 "right": false
-            }]
-        },{
+            }],
+            get ques() {
+
+                return this.questions
+            },
+        }, {
             'value': 200,
             'question': "Jim sends faxes to Dwight from who?",
             "answers": [{
@@ -62,7 +67,7 @@ let officeCat =
                 "d": "Future Dwight",
                 "right": true
             }]
-        },{
+        }, {
             'value': 300,
             'question': "Where does Jim relocate Dwight's desk to?",
             "answers": [{
@@ -78,7 +83,7 @@ let officeCat =
                 "d": "The elevator",
                 "right": false
             }]
-        },{
+        }, {
             'value': 400,
             'question': "Jim dresses up as who to freak Dwight out?",
             "answers": [{
@@ -94,7 +99,176 @@ let officeCat =
                 "d": "The Devil",
                 "right": false
             }]
-        },{
+        }, {
+            'value': 500,
+            'question': "Jim convinces Dwight he's being recruited by who?",
+            "answers": [{
+                "one": "Secret Service",
+                "right": false
+            }, {
+                "b": "FBI",
+                "right": false
+            }, {
+                "c": "CIA",
+                "right": true
+            }, {
+                "d": "PETA",
+                "right": false
+            }]
+        }
+        ]
+    },
+    {
+        'name': 'mikescott',
+        'questions': [{
+            'value': 100,
+            'question': "Who's car is hit with a watermelon during the safety training episode?",
+            "answers": [{
+                "a": "Stanley",
+                "right": true
+            }, {
+                "b": "Jello",
+                "right": true
+            }, {
+                "c": "Pudding",
+                "right": false
+            }, {
+                "d": "Cake",
+                "right": false
+            }]
+        }, {
+            'value': 200,
+            'question': "Jim sends faxes to Dwight from who?",
+            "answers": [{
+                "a": "Abe Lincoln",
+                "right": false
+            }, {
+                "b": "A high ranking official from the FBI",
+                "right": false
+            }, {
+                "c": "Hitler",
+                "right": false
+            }, {
+                "d": "Future Dwight",
+                "right": true
+            }]
+        }, {
+            'value': 300,
+            'question': "Where does Jim relocate Dwight's desk to?",
+            "answers": [{
+                "a": "The men's bathroom",
+                "right": true
+            }, {
+                "b": "The women's bathroom",
+                "right": false
+            }, {
+                "c": "The roof",
+                "right": false
+            }, {
+                "d": "The elevator",
+                "right": false
+            }]
+        }, {
+            'value': 400,
+            'question': "Jim dresses up as who to freak Dwight out?",
+            "answers": [{
+                "one": "Dwight",
+                "right": true
+            }, {
+                "b": "The Sith Lord",
+                "right": false
+            }, {
+                "c": "Pimp",
+                "right": false
+            }, {
+                "d": "The Devil",
+                "right": false
+            }]
+        }, {
+            'value': 500,
+            'question': "Jim convinces Dwight he's being recruited by who?",
+            "answers": [{
+                "one": "Secret Service",
+                "right": false
+            }, {
+                "b": "FBI",
+                "right": false
+            }, {
+                "c": "CIA",
+                "right": true
+            }, {
+                "d": "PETA",
+                "right": false
+            }]
+        }
+        ]
+    }, {
+        'name': 'mikescott',
+        'questions': [{
+            'value': 100,
+            'question': "Who's car is hit with a watermelon during the safety training episode?",
+            "answers": [{
+                "a": "Stanley",
+                "right": true
+            }, {
+                "b": "Jello",
+                "right": true
+            }, {
+                "c": "Pudding",
+                "right": false
+            }, {
+                "d": "Cake",
+                "right": false
+            }]
+        }, {
+            'value': 200,
+            'question': "Jim sends faxes to Dwight from who?",
+            "answers": [{
+                "a": "Abe Lincoln",
+                "right": false
+            }, {
+                "b": "A high ranking official from the FBI",
+                "right": false
+            }, {
+                "c": "Hitler",
+                "right": false
+            }, {
+                "d": "Future Dwight",
+                "right": true
+            }]
+        }, {
+            'value': 300,
+            'question': "Where does Jim relocate Dwight's desk to?",
+            "answers": [{
+                "a": "The men's bathroom",
+                "right": true
+            }, {
+                "b": "The women's bathroom",
+                "right": false
+            }, {
+                "c": "The roof",
+                "right": false
+            }, {
+                "d": "The elevator",
+                "right": false
+            }]
+        }, {
+            'value': 400,
+            'question': "Jim dresses up as who to freak Dwight out?",
+            "answers": [{
+                "one": "Dwight",
+                "right": true
+            }, {
+                "b": "The Sith Lord",
+                "right": false
+            }, {
+                "c": "Pimp",
+                "right": false
+            }, {
+                "d": "The Devil",
+                "right": false
+            }]
+        }, {
             'value': 500,
             'question': "Jim convinces Dwight he's being recruited by who?",
             "answers": [{
@@ -113,45 +287,57 @@ let officeCat =
         }
         ]
     }
-console.log(officeCat)
+]
 
-    const allquestions = () =>{
-        for(i=0; i<officeCat.questions.length; i++){
-            // console.log(officeCat.questions[i])
-            // console.log(officeCat.questions[i].answers)
-        }
-        // console.log(officeCat.questions[i])
-    }
-    $('#prank1').click(()=>{
-        console.log(officeCat.questions[0])
-        // allquestions()
-    })
+$('#prank1').click(() => {
+    $('.gameboard').css("visibility", "hidden")
+    console.log(officeCat[0].questions[0])
+    $('p').css('display', 'flex')
+    $('p').data(officeCat[0].questions[0])
+})
 
-    $('#prank2').click(()=>{
-        console.log(officeCat.questions[1])
-        // allquestions()
-    })
+$('#prank2').click(() => {
+    console.log(officeCat[0].questions[1])
+})
 
-    $('#prank3').click(()=>{
-        console.log(officeCat.questions[2])
-        // allquestions()
-    })
+$('#prank3').click(() => {
+    console.log(officeCat[0].questions[2])
+})
 
-    $('#prank4').click(()=>{
-        console.log(officeCat.questions[3])
-        // allquestions()
-    })
+$('#prank4').click(() => {
+    console.log(officeCat[0].questions[3])
+})
 
-    $('#prank5').click(()=>{
-        console.log(officeCat.questions[4])
-        // allquestions()
-    })
+$('#prank5').click(() => {
+    console.log(officeCat[0].questions[4])
+})
+// category 2
+$('#ms1').click(() => {
+    console.log(officeCat[1].questions[0])
+})
 
-    $(".open").on("click", function(){
-        $(".popup-overlay, .popup-content").addClass("active");
-      });
-      
-      //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
-      $(".close, .popup-overlay").on("click", function(){
-        $(".popup-overlay, .popup-content").removeClass("active");
-      });
+$('#ms2').click(() => {
+    console.log(officeCat[1].questions[1])
+})
+
+$('#ms3').click(() => {
+    console.log(officeCat[1].questions[2])
+})
+
+$('#ms4').click(() => {
+    $('.gameboard').hide()
+    console.log(officeCat[1].questions[3])
+})
+
+$('#ms5').click(() => {
+    console.log(officeCat[1].questions[4])
+})
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+$(".questions").on("click", () => {
+    $(".popup-overlay, .popup-content").addClass("active");
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+$(".close, .popup-overlay").on("click", function () {
+    $(".popup-overlay, .popup-content").removeClass("active");
+});
