27ab4706864ccac5a6e023c48ab7a719d33c7117
Andrea Jemison <andreadjemison@yahoo.com>
2019-04-15 07:12:17 -0400

    questions added

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ let officeCat = [
         }
         ]
     }, {
-        'name': 'mikescott',
+        'name': 'officeromance',
         'questions': [{
             'value': 100,
             'question': "Who's car is hit with a watermelon during the safety training episode?",
@@ -284,10 +284,262 @@ let officeCat = [
                 "d": "PETA",
                 "right": false
             }]
-        }
-        ]
-    }
-]
+        },
+        {
+            'name': 'employees',
+            'questions': [{
+                'value': 100,
+                'question': "Who's car is hit with a watermelon during the safety training episode?",
+                "answers": [{
+                    "a": "Stanley",
+                    "right": true
+                }, {
+                    "b": "Jello",
+                    "right": true
+                }, {
+                    "c": "Pudding",
+                    "right": false
+                }, {
+                    "d": "Cake",
+                    "right": false
+                }]
+            }, {
+                'value': 200,
+                'question': "Jim sends faxes to Dwight from who?",
+                "answers": [{
+                    "a": "Abe Lincoln",
+                    "right": false
+                }, {
+                    "b": "A high ranking official from the FBI",
+                    "right": false
+                }, {
+                    "c": "Hitler",
+                    "right": false
+                }, {
+                    "d": "Future Dwight",
+                    "right": true
+                }]
+            }, {
+                'value': 300,
+                'question': "Where does Jim relocate Dwight's desk to?",
+                "answers": [{
+                    "a": "The men's bathroom",
+                    "right": true
+                }, {
+                    "b": "The women's bathroom",
+                    "right": false
+                }, {
+                    "c": "The roof",
+                    "right": false
+                }, {
+                    "d": "The elevator",
+                    "right": false
+                }]
+            }, {
+                'value': 400,
+                'question': "Jim dresses up as who to freak Dwight out?",
+                "answers": [{
+                    "one": "Dwight",
+                    "right": true
+                }, {
+                    "b": "The Sith Lord",
+                    "right": false
+                }, {
+                    "c": "Pimp",
+                    "right": false
+                }, {
+                    "d": "The Devil",
+                    "right": false
+                }]
+            }, {
+                'value': 500,
+                'question': "Jim convinces Dwight he's being recruited by who?",
+                "answers": [{
+                    "one": "Secret Service",
+                    "right": false
+                }, {
+                    "b": "FBI",
+                    "right": false
+                }, {
+                    "c": "CIA",
+                    "right": true
+                }, {
+                    "d": "PETA",
+                    "right": false
+                }]
+            }
+            ]
+        }, {
+            'name': 'quotes',
+            'questions': [{
+                'value': 100,
+                'question': 'As a gay man, Im horrified. As a friend of Angelas, horrified. As a lover of elegant weddings, Im a little excited.',
+                "answers": [{
+                    "a": "Andy",
+                    "right": true
+                }, {
+                    "b": "Oscar",
+                    "right": true
+                }, {
+                    "c": "Michael",
+                    "right": false
+                }, {
+                    "d": "Toby",
+                    "right": false
+                }]
+            }, {
+                'value': 200,
+                'question': "All you need is love. False. The four basic human necessities are air, water, food, and shelter.",
+                "answers": [{
+                    "a": "Creed",
+                    "right": false
+                }, {
+                    "b": "Toby",
+                    "right": false
+                }, {
+                    "c": "Darryl",
+                    "right": false
+                }, {
+                    "d": "Dwight",
+                    "right": true
+                }]
+            }, {
+                'value': 300,
+                'question': "Every little boy fantasizes about his fairytale wedding.",
+                "answers": [{
+                    "a": "Andy",
+                    "right": true
+                }, {
+                    "b": "Oscar",
+                    "right": false
+                }, {
+                    "c": "Michael",
+                    "right": false
+                }, {
+                    "d": "Ryan",
+                    "right": false
+                }]
+            }, {
+                'value': 400,
+                'question': "In five years, I'd like to be five years sober.",
+                "answers": [{
+                    "one": "Meredith",
+                    "right": true
+                }, {
+                    "b": "Toby",
+                    "right": false
+                }, {
+                    "c": "Jim",
+                    "right": false
+                }, {
+                    "d": "Creed",
+                    "right": false
+                }]
+            }, {
+                'value': 500,
+                'question': "Thats what she said",
+                "answers": [{
+                    "one": "Andy",
+                    "right": false
+                }, {
+                    "b": "Darryl",
+                    "right": false
+                }, {
+                    "c": "Michael",
+                    "right": true
+                }, {
+                    "d": "Jim",
+                    "right": false
+                }]
+            }
+            ]
+        }, {
+            'name': 'management',
+            'questions': [{
+                'value': 100,
+                'question': "Who's car is hit with a watermelon during the safety training episode?",
+                "answers": [{
+                    "a": "Stanley",
+                    "right": true
+                }, {
+                    "b": "Jello",
+                    "right": true
+                }, {
+                    "c": "Pudding",
+                    "right": false
+                }, {
+                    "d": "Cake",
+                    "right": false
+                }]
+            }, {
+                'value': 200,
+                'question': "Jim sends faxes to Dwight from who?",
+                "answers": [{
+                    "a": "Abe Lincoln",
+                    "right": false
+                }, {
+                    "b": "A high ranking official from the FBI",
+                    "right": false
+                }, {
+                    "c": "Hitler",
+                    "right": false
+                }, {
+                    "d": "Future Dwight",
+                    "right": true
+                }]
+            }, {
+                'value': 300,
+                'question': "Where does Jim relocate Dwight's desk to?",
+                "answers": [{
+                    "a": "The men's bathroom",
+                    "right": true
+                }, {
+                    "b": "The women's bathroom",
+                    "right": false
+                }, {
+                    "c": "The roof",
+                    "right": false
+                }, {
+                    "d": "The elevator",
+                    "right": false
+                }]
+            }, {
+                'value': 400,
+                'question': "Jim dresses up as who to freak Dwight out?",
+                "answers": [{
+                    "one": "Dwight",
+                    "right": true
+                }, {
+                    "b": "The Sith Lord",
+                    "right": false
+                }, {
+                    "c": "Pimp",
+                    "right": false
+                }, {
+                    "d": "The Devil",
+                    "right": false
+                }]
+            }, {
+                'value': 500,
+                'question': "Jim convinces Dwight he's being recruited by who?",
+                "answers": [{
+                    "one": "Secret Service",
+                    "right": false
+                }, {
+                    "b": "FBI",
+                    "right": false
+                }, {
+                    "c": "CIA",
+                    "right": true
+                }, {
+                    "d": "PETA",
+                    "right": false
+                }]
+            }
+            ]
+        }]
+    }]
+
 
 $('#prank1').click(() => {
     $('.gameboard').css("visibility", "hidden")
